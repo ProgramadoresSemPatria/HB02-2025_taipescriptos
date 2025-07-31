59128720b33bbee3a73bcba6f4ddcc788b90dfa9
Kauê <kauevecchia@hotmail.com>
2025-07-30 23:04:39 -0300

    refactor: replace Sidebar with AppSidebar in AppLayout for improved component structure

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom'
-import { Sidebar } from '../components/Sidebar'
+import { AppSidebar } from '@/components/Sidebar'
 
 export function AppLayout() {
   return (
     <div className="h-screen flex flex-col bg-background">
       <div className="flex-grow flex">
-        <aside className="lg:w-1/5 hidden lg:block">
-          <Sidebar />
+        <aside>
+          <AppSidebar />
         </aside>
         <main className="p-4 flex-grow min-w-0">
           <Outlet />
