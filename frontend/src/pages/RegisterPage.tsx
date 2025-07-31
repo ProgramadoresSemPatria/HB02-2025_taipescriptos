942568929442ba8d565c6b695984006f931d3b4f
Nathan <viniciosnathan@gmail.com>
2025-07-31 17:40:04 -0300

    feat: add ReturnButton to RegisterPage for improved navigation

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import ReturnButton from '@/components/ui/ReturnButton'
 import RegisterForm from '@/components/auth/RegisterForm'
 
 const RegisterPage = () => {
@@ -6,6 +6,10 @@ const RegisterPage = () => {
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <RegisterForm />
+      </div>
+      <div className="absolute top-4 left-4">
+        {' '}
+        <ReturnButton />
       </div>
     </div>
   )
