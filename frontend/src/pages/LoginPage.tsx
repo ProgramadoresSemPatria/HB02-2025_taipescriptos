ed9a2843cead57428ec442b5479a5e6c8ba43ba3
Nathan <viniciosnathan@gmail.com>
2025-07-31 17:39:55 -0300

    feat: add ReturnButton to LoginPage for improved navigation

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,3 +1,4 @@
+import ReturnButton from '@/components/ui/ReturnButton'
 import LoginForm from '@/components/auth/LoginForm'
 
 const LoginPage = () => {
@@ -5,6 +6,10 @@ const LoginPage = () => {
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <LoginForm />
+      </div>
+      <div className="absolute top-4 left-4">
+        {' '}
+        <ReturnButton />
       </div>
     </div>
   )
