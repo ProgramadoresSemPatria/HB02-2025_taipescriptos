b008f34d52936fbac074e8ae240e7ae3ded05fa1
Kauê <kauevecchia@hotmail.com>
2025-07-31 22:35:34 -0300

    refactor: update AppLayout to use SidebarInset and apply custom styles

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -1,22 +1,25 @@
 import { Outlet } from 'react-router-dom'
 import { AppSidebar } from '@/components/Sidebar'
-import { SidebarProvider } from '@/components/ui/sidebar'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 import { SiteHeader } from '@/components/SiteHeader'
 
 export function AppLayout() {
   return (
-    <SidebarProvider>
-      <div className="h-screen flex flex-col bg-background">
-        <div className="flex-grow flex">
-          <aside>
-            <AppSidebar />
-          </aside>
-          <main className="p-4 flex-grow min-w-0">
-            <SiteHeader />
-            <Outlet />
-          </main>
-        </div>
-      </div>
+    <SidebarProvider
+      style={
+        {
+          '--sidebar-width': 'calc(var(--spacing) * 72)',
+          '--header-height': 'calc(var(--spacing) * 12)',
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <main className="flex-1 px-8 py-4 md:px-12 md:py-8 min-w-0">
+          <Outlet />
+        </main>
+      </SidebarInset>
     </SidebarProvider>
   )
 }
